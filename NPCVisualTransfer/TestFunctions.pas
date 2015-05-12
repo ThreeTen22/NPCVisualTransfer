cc8e6f9e51dc0179a16dcf10262db0d4a1827a25
Grant <gtumen@gmail.com>
2015-05-12 10:50:54 -0500

    Added DeleteReleventRecords()
    This will go through a formalist and remove all elements contained from PatchFile.  Used to quickly clean up NPCs.

--- NPCVisualTransfer/TestFunctions.pas
+++ NPCVisualTransfer/TestFunctions.pas
@@ -1,1 +1,21 @@
-
+procedure DeleteReleventRecords(iNPCToDelete: IInterface);
+var
+i,s: integer;
+ev: string;
+iGRUP, iFLST, iFormIDs iElement: IInterface;
+begin
+  iGRUP := GroupBySignature(PatchFile, 'FLST'):
+  for i:= 0 to Pred(ElementCount(iGRUP)) do begin
+    iFLST := ElementByIndex(iGRUP, i);
+    ev := geev(iFLST,'FormIDs\[0]');
+    if ev = HexFormID(iNPCToDelete) then break;
+    ev := ''
+  end;
+  if ev = '' then exit;
+  iFormIDs := ElementByIP(iFLST 'FormIDs');
+  while ElementCount(iFormIDs > 0) do begin 
+    iElement := RemoveByIndex(iFormIDs, 0);
+    Remove(iElement);
+  end;
+  Remove(iFLST);
+end;
